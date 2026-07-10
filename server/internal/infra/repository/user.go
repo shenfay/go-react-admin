533cb1891c6ee031d2591b70dd4d57ff3de953c8
shenfay <shengfai@qq.com>
2026-07-10 09:02:37 +0800

    refactor(server): TimeNull 类型提取到独立 types.go
    - 将 TimeNull 可空时间类型从 user.go 提取到 repository/types.go
    - 该类型被 user/role/menu 三个 repository 共用，提取后职责更清晰
    - 清理 user.go 中不再需要的 database/sql/driver、encoding/json、fmt 等导入

--- server/internal/infra/repository/user.go
+++ server/internal/infra/repository/user.go
@@ -2,17 +2,13 @@ package repository
 
 import (
 	"context"
-	"database/sql/driver"
-	"encoding/json"
 	"errors"
-	"fmt"
 	"time"
 
 	"gorm.io/gorm"
 
 	"github.com/shenfay/kiqi/internal/domain/user"
 	userErr "github.com/shenfay/kiqi/pkg/errors/user"
-	"github.com/shenfay/kiqi/pkg/utils"
 )
 
 // UserPO 用户持久化对象
@@ -32,73 +28,6 @@ type UserPO struct {
 // TableName 指定表名为 users
 func (UserPO) TableName() string {
 	return "users"
-}
-
-// TimeNull 可空的时间类型
-type TimeNull struct {
-	Time  time.Time
-	Valid bool
-}
-
-// Value 实现 driver.Valuer 接口
-func (t TimeNull) Value() (driver.Value, error) {
-	if !t.Valid {
-		return nil, nil
-	}
-	return t.Time, nil
-}
-
-// Scan 实现 sql.Scanner 接口
-func (t *TimeNull) Scan(value interface{}) error {
-	if value == nil {
-		t.Time = time.Time{}
-		t.Valid = false
-		return nil
-	}
-
-	switch v := value.(type) {
-	case time.Time:
-		t.Time = v
-		t.Valid = true
-		return nil
-	default:
-		return fmt.Errorf("failed to scan TimeNull: %v", value)
-	}
-}
-
-// MarshalJSON 实现 JSON 序列化
-func (t TimeNull) MarshalJSON() ([]byte, error) {
-	if t.Valid {
-		return json.Marshal(utils.FormatRFC3339(t.Time))
-	}
-	return json.Marshal(nil)
-}
-
-// UnmarshalJSON 实现 JSON 反序列化
-func (t *TimeNull) UnmarshalJSON(data []byte) error {
-	var s interface{}
-	if err := json.Unmarshal(data, &s); err != nil {
-		return err
-	}
-
-	if s == nil {
-		t.Valid = false
-		return nil
-	}
-
-	str, ok := s.(string)
-	if !ok {
-		return fmt.Errorf("invalid time value")
-	}
-
-	parsed, err := utils.ParseRFC3339(str)
-	if err != nil {
-		return err
-	}
-
-	t.Time = parsed
-	t.Valid = true
-	return nil
 }
 
 // ToDomain 转换为领域模型
